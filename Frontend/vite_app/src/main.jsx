ea556d278d0ab5f37c5b59ed0bfe58c23073554c
Kanishka-Trivedi <kally651997@gmail.com>
2025-02-05 12:21:26 +0530

    Goals Section Added

--- Frontend/vite_app/src/main.jsx
+++ Frontend/vite_app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import Dashboard from "./Dashboard.jsx"
+import Goals from "./Goals.jsx"
+import App from "./App.jsx"
 import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Dashboard />
+    <App />
   </React.StrictMode>,
 )
 
