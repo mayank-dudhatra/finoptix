45fea35818632dac011f299aa5f37de6e75a1e23
Kanishka-Trivedi <kally651997@gmail.com>
2025-03-03 18:54:33 +0530

    Landing and Signin page added

--- Frontend/vite_app/src/App.jsx
+++ Frontend/vite_app/src/App.jsx
@@ -6,6 +6,8 @@ import Your_Budget from "./Your_Budget";
 import InvestmentAnalytics from "./InvestmentAnalytics";
 import Profile from "./Profile";
 import NotFound from "./Components/NotFound";
+import Landing from "./Landing";
+import SignIn from "./SignIn";
 
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
         <Route path="/analytics" element={<InvestmentAnalytics />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/*" element={<NotFound />} /> {/* Catch-all 404 route */}
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </Router>
   );
